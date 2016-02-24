cefb595804787a741cd5d7e1153def1b9cfaa761
Chris Grimmett <chris@grimtech.net>
2016-02-24 09:27:51 -0800

    clean out testy

--- app/routes/index.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-
-export default Ember.Route.extend({
-  model() {
-    console.log('test')
-    return this.store.findAll('minion');
-  }
-});
--- app/routes/scientists.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model() {
-    return ['Donkie Hotay', 'Lady Adie', 'Feliz Cupleanos', 'Ultra Tor'];
-  }
-});
